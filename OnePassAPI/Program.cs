65439e4297f7cf35c2442345f7b6f6e36ef96c3c
Nishant <sinha.nishant@outlook.com>
2025-11-18 19:28:09 +0530

    Enabling cors in controller

--- OnePassAPI/Program.cs
+++ OnePassAPI/Program.cs
@@ -66,8 +66,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAll",
         policy =>
         {
-            policy.AllowAnyOrigin()
-                  .AllowAnyHeader()
+            policy.WithOrigins("http://localhost:3000")
+                      .AllowAnyHeader()
                   .AllowAnyMethod();
         });
 });
