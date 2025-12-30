a76e501cc81e0a5981e48809709a893914359cec
Nishant <sinha.nishant@outlook.com>
2025-12-30 18:49:29 +0530

    facematch

---
+++ OnePass.Domain/Entitites/Domain/Hotel/FaceMatchStatusRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePass.Domain
+{
+    public class FaceMatchStatusRequest
+    {
+        public string BookingId { get; set; }
+        public string PhoneCountryCode { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+}
---
+++ OnePass.Domain/Entitites/Domain/Response/Hotel/FaceMatchStatusResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePass.Domain
+{
+    public class FaceMatchStatusResponse
+    {
+        public string BookingId { get; set; }
+        public string PhoneCountryCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsFaceMatched { get; set; }
+        public double FaceMatchScore { get; set; }
+    }
+
+}
